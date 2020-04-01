7dbe1ce01ff8125d94856c3dd28dd5b6e090c780
seahawks97 <40189420+seahawks97@users.noreply.github.com>
2020-04-01 11:26:42 -0400

    added modifiers
    NOT NULL, default CURRENT_TIMESTAMP, changed some variables VARCHAR sizes

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,33 +4,33 @@ drop table IF EXISTS user_info;
 drop table IF EXISTS tag;
 drop table IF EXISTS tile;
 
-CREATE TABLE users 
+CREATE TABLE SS_users 
     (UID            VARCHAR(24) NOT NULL,
-     joined         DATETIME,
-     last_login     DATETIME,
-     profile_pic    VARCHAR(24),
-     PRIMARY KEY(UID)) ENGINE=INNODB;
+    joined          DATETIME default CURRENT_TIMESTAMP,
+    last_login      DATETIME default CURRENT_TIMESTAMP,
+    profile_pic     VARCHAR(24),
+    PRIMARY KEY(UID)) ENGINE=INNODB;
 
-CREATE TABLE user_info
-    (email          VARCHAR(32),
-     password       VARCHAR(24) NOT NULL,
+CREATE TABLE SS_user_info
+    (email          VARCHAR(32) NOT NULL,
+    password        VARCHAR(64) NOT NULL,
     PRIMARY KEY(email)) ENGINE=INNODB;
     
-CREATE TABLE tag
+CREATE TABLE SS_tag
     (UID            VARCHAR(24) NOT NULL,
-    tagname         VARCHAR(32),
-    description     VARCHAR(32),
-    time            DATETIME,
+    tagname         VARCHAR(32) NOT NULL,
+    description     VARCHAR(64),
+    time            DATETIME default CURRENT_TIMESTAMP,
     PRIMARY KEY(UID, tagname)
     FOREIGN KEY(UID) REFERENCES users(UID) ON DELETE SET NULL) ENGINE=INNODB;
     
-CREATE TABLE tile
-    (tile_name      VARCHAR(24),
-    UID             VARCHAR(24),
-    tag_name        VARCHAR(32),
-    description     VARCHAR(32),
-    url             VARCHAR(32),
-    time            DATETIME,
+CREATE TABLE SS_tile
+    (tile_name      VARCHAR(24) NOT NULL,
+    UID             VARCHAR(24) NOT NULL,
+    tag_name        VARCHAR(32) NOT NULL,
+    description     VARCHAR(64),
+    url             VARCHAR(32) NOT NULL,
+    time            DATETIME default CURRENT_TIMESTAMP,
     PRIMARY KEY(tile_name, UID, tag_name)
     FOREIGN KEY(UID) REFERENCES users(UID) ON DELETE SET NULL)
     FOREIGN KEY(tag_name) REFERENCES tag(tag_name) ON DELETE SET NULL)) ENGINE=INNODB;

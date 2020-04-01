75ef2c36516223bc36ec1e3da2f4a861d0aae9ee
seahawks97 <40189420+seahawks97@users.noreply.github.com>
2020-04-01 15:33:05 -0400

    fixed foreign key issue in SS_tiles

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,36 +1,38 @@
 use snt8550;
+drop table IF EXISTS SS_tiles;
+drop table IF EXISTS SS_tags;
 drop table IF EXISTS SS_users;
 drop table IF EXISTS SS_user_info;
-drop table IF EXISTS SS_tag;
-drop table IF EXISTS SS_tile;
 
 CREATE TABLE SS_users 
-    (UID            VARCHAR(24) NOT NULL,
-    joined          DATETIME default CURRENT_TIMESTAMP,
-    last_login      DATETIME default CURRENT_TIMESTAMP,
-    profile_pic     VARCHAR(24),
-    PRIMARY KEY(UID)) ENGINE=INNODB;
+    (userID      VARCHAR(24) NOT NULL,
+    joined       DATETIME default CURRENT_TIMESTAMP,
+    last_login   DATETIME default CURRENT_TIMESTAMP,
+    profile_pic  VARCHAR(24) default NULL,
+    PRIMARY KEY (userID)) ENGINE=INNODB;
 
 CREATE TABLE SS_user_info
-    (email          VARCHAR(32) NOT NULL,
-    password        VARCHAR(64) NOT NULL,
-    PRIMARY KEY(email)) ENGINE=INNODB;
-    
-CREATE TABLE SS_tag
-    (UID            VARCHAR(24) NOT NULL,
-    tagname         VARCHAR(32) NOT NULL,
-    description     VARCHAR(64),
-    time            DATETIME default CURRENT_TIMESTAMP,
-    PRIMARY KEY(UID, tagname)
-    FOREIGN KEY(UID) REFERENCES users(UID) ON DELETE SET NULL) ENGINE=INNODB;
-    
-CREATE TABLE SS_tile
-    (tile_name      VARCHAR(24) NOT NULL,
-    UID             VARCHAR(24) NOT NULL,
-    tag_name        VARCHAR(32) NOT NULL,
-    description     VARCHAR(64),
-    url             VARCHAR(32) NOT NULL,
-    time            DATETIME default CURRENT_TIMESTAMP,
-    PRIMARY KEY(tile_name, UID, tag_name)
-    FOREIGN KEY(UID) REFERENCES users(UID) ON DELETE SET NULL)
-    FOREIGN KEY(tag_name) REFERENCES tag(tag_name) ON DELETE SET NULL)) ENGINE=INNODB;
+    (email    VARCHAR(32),
+    password  VARCHAR(64),
+    PRIMARY KEY (email)) ENGINE=INNODB;
+
+CREATE TABLE SS_tags
+    (userID       VARCHAR(24) NOT NULL,
+	tag_name      VARCHAR(32) NOT NULL,
+    description   VARCHAR(128),
+    time_created  DATETIME default CURRENT_TIMESTAMP,
+    PRIMARY KEY (userID, tag_name),
+    FOREIGN KEY (userID) REFERENCES SS_users(userID)) ENGINE=INNODB;
+	
+ALTER TABLE `SS_tags` ADD INDEX(`tag_name`);
+
+CREATE TABLE SS_tiles
+    (userID       VARCHAR(24) NOT NULL,
+    tag_name      VARCHAR(32) NOT NULL,
+    tile_name     VARCHAR(32) NOT NULL,
+    description   VARCHAR(128),
+    url           VARCHAR(64) NOT NULL,
+    time_created  DATETIME default CURRENT_TIMESTAMP,
+    PRIMARY KEY (userID, tag_name, tile_name),
+    FOREIGN KEY (userID) REFERENCES SS_users(userID),
+	FOREIGN KEY (tag_name) REFERENCES SS_tags(tag_name)) ENGINE=INNODB;

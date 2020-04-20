708de5ff411852b1dcef508835deda1376da608b
kmt8181 <60239458+kmt8181@users.noreply.github.com>
2020-04-20 00:20:49 -0400

    Add files via upload

---
+++ user_tags.php
@@ -0,0 +1,14 @@
+<h3>Users Tags</h3>
+            
+        <section id="tags_description">
+                <p>This page displays all of a users tags</p>
+        </section>
+<?php
+    $sql="'SELECT tag_name FROM SS_tags WHERE userID = ?';"
+
+$result = mysql_query($sql);
+while($row = mysql_fetch_array($result)) {
+    echo $row['fieldname']; 
+}
+
+?>

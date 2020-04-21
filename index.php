c4ff3b7cb538ad381407a0c7054c579c69d5ddfc
kmt8181 <60239458+kmt8181@users.noreply.github.com>
2020-04-21 14:35:05 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php include('includes/header.php'); ?>
+<?php   include('includes/header.php');
+        include('includes/nav-bar.php');?>
 
 		<section id="description">
 			<p>This site allows a user to create a custom array of sites they may commonly use with tags such as <a href="#">"Memes"</a>, <a href="#">"Food"</a>, or <a href="#">"Sports"</a>. There will be the homepage, user pages, and tag pages. The user page will contain a list of tags that will take the user to the tag page when clicked. the tag page will have an aside element with a list of the user's tags followed by a brief description of the tag page. The tag page will have a 2-by-n or 3-by-n array of "tiles" about the same size as the header that will take the user to a website that it is linked to. There will be several additional options that a user can operate to change the appearance of the tile.</p>

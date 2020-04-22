c9123f384aa2fced0922a1f8fa79b55a47fa2a22
kmt8181 <60239458+kmt8181@users.noreply.github.com>
2020-04-22 11:43:02 -0400

    Add files via upload

---
+++ nav-bar.php
@@ -0,0 +1,17 @@
+<header class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="#" class="navbar-brand"></a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></button>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>   
+                <li><a href="index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="#">Recent Post</a></li>
+            </ul>
+        </div>
+    </div>
+</header>

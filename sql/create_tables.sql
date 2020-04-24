2542fa97226feccfbf4d764511e88e5e08653bc4
seahawks97 <40189420+seahawks97@users.noreply.github.com>
2020-04-23 23:58:36 -0400

    added cascade specifications

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,13 +3,14 @@ drop table IF EXISTS SS_tiles;
 drop table IF EXISTS SS_tags;
 drop table IF EXISTS SS_users;
 
-CREATE TABLE SS_users 
+CREATE TABLE SS_users
     (userID      VARCHAR(24) NOT NULL,
 	email        VARCHAR(32) NOT NULL,
 	password     VARCHAR(64) NOT NULL,
     joined       DATETIME default CURRENT_TIMESTAMP,
     last_login   DATETIME default CURRENT_TIMESTAMP,
     profile_pic  VARCHAR(24) default NULL,
+	is_admin     INT default '0' NOT NULL,
     PRIMARY KEY (userID)) ENGINE=INNODB;
 
 CREATE TABLE SS_tags
@@ -18,8 +19,11 @@ CREATE TABLE SS_tags
     description   VARCHAR(128),
     time_created  DATETIME default CURRENT_TIMESTAMP,
     PRIMARY KEY (userID, tag_name),
-    FOREIGN KEY (userID) REFERENCES SS_users(userID)) ENGINE=INNODB;
-	
+    FOREIGN KEY (userID) 
+		REFERENCES SS_users(userID)
+		ON DELETE CASCADE
+	) ENGINE=INNODB;
+
 ALTER TABLE `SS_tags` ADD INDEX(`tag_name`);
 
 CREATE TABLE SS_tiles
@@ -30,5 +34,10 @@ CREATE TABLE SS_tiles
     url           VARCHAR(64) NOT NULL,
     time_created  DATETIME default CURRENT_TIMESTAMP,
     PRIMARY KEY (userID, tag_name, tile_name),
-    FOREIGN KEY (userID) REFERENCES SS_users(userID),
-	FOREIGN KEY (tag_name) REFERENCES SS_tags(tag_name)) ENGINE=INNODB;
+    FOREIGN KEY (userID) 
+		REFERENCES SS_users(userID)
+		ON DELETE CASCADE,
+	FOREIGN KEY (tag_name) 
+		REFERENCES SS_tags(tag_name)
+		ON DELETE CASCADE
+	) ENGINE=INNODB;

8f9146186c1c19c82b64fdc51d86df380282d3be
seahawks97 <40189420+seahawks97@users.noreply.github.com>
2020-04-25 19:37:43 -0400

    FINISHED STORED FUNCTIONS AND TRIGGER
    yay

--- sql/extra_functions.sql
+++ sql/extra_functions.sql
@@ -1,12 +1,15 @@
 -- Trigger
-CREATE TRIGGER user_check 
-BEFORE INSERT ON SS_users
-FOR EACH ROW
+DELIMITER $$
+
+CREATE TRIGGER my_first_tag
+AFTER INSERT
+ON SS_users FOR EACH ROW
 BEGIN
-    IF(EXISTS(SELECT 1 FROM SS_users WHERE userID = NEW.userID)) THEN SIGNAL SQLSTATE VALUE '45000' SET MESSAGE_TEXT = 'INSERT failed due to duplicate user id';
-    END IF;
+    INSERT INTO SS_tags(userID, tag_name, description)
+    VALUES(NEW.userID, 'My first tag!!', 'Welcome to Site Sight.  We hope you love it!!');
 END$$
-DELIMITER;
+
+DELIMITER ;
 
 
 
@@ -27,21 +30,15 @@ FROM SS_tags;
 
 
 -- Stored Function
--- Called like 'SELECT to_url(viewSS_users, userID) FROM SS_users'  OR  'SELECT to_url(SS_tags, tag_name) FROM SS_tags';
-CREATE FUNCTION to_url (
-	view_name VARCHAR(16),		-- either "viewSS_users" or "viewSS_tags" in function call
-	space_str VARCHAR(32)		-- either "userID" or "tag_name" in function call
-)
-RETURNS VARCHAR(32)
-BEGIN
-	DECLARE newStr VARCHAR(32);
-	
-	IF (view_name == "SS_users" OR view_name == "SS_tags") THEN
-		SET newStr = REPLACE(space_str, ' ', '_');
-	
-	END IF;
-	
-	RETURN (newStr);
+-- Called like 'SELECT userID, to_url(userID) FROM SS_users'
+DROP FUNCTION to_url;
 
-END
-Delimiter ;
+DELIMITER $$
+CREATE FUNCTION to_url(space_str VARCHAR(32))
+RETURNS VARCHAR(32) 
+BEGIN 
+	DECLARE newStr VARCHAR(32);
+	SET newStr = REPLACE(space_str, ' ', '_');
+	RETURN (newStr);
+END$$
+DELIMITER ;
